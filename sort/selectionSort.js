b477303ec6d7c860bde5ee6212bdcaab418d805d
yingjieweb <1184061078@qq.com>
2020-03-14 09:51:35 +0800

    selectionSort

--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -4,12 +4,12 @@
  * @return {Array}
  */
 function selectionSort(arr) {
-    var len = arr.length;
-    var minIndex, temp;
+    let len = arr.length;
+    let minIndex, temp;
     console.time('选择排序耗时');
-    for (var i = 0; i < len - 1; i++) {
+    for (let i = 0; i < len - 1; i++) {
         minIndex = i;
-        for (var j = i + 1; j < len; j++) {
+        for (let j = i + 1; j < len; j++) {
             if (arr[j] < arr[minIndex]) { //寻找最小的数
                 minIndex = j; //将最小数的索引保存
             }
@@ -21,5 +21,5 @@ function selectionSort(arr) {
     console.timeEnd('选择排序耗时');
     return arr;
 }
-var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+let arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 console.log(selectionSort(arr)); //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
