8b9cb3aa01f1f5c874362e573242327014b59034
yingjieweb <1184061078@qq.com>
2020-03-14 09:51:09 +0800

    quickSort

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -8,10 +8,10 @@ function quickSort(array, left, right) {
     console.time('1.快速排序耗时');
     if (Object.prototype.toString.call(array).slice(8, -1) === 'Array' && typeof left === 'number' && typeof right === 'number') {
         if (left < right) {
-            var x = array[right],
+            let x = array[right],
                 i = left - 1,
                 temp;
-            for (var j = left; j <= right; j++) {
+            for (let j = left; j <= right; j++) {
                 if (array[j] <= x) {
                     i++;
                     temp = array[i];
@@ -30,16 +30,16 @@ function quickSort(array, left, right) {
 }
 
 //方法二
-var quickSort2 = function(arr) {
+let quickSort2 = function(arr) {
     console.time('2.快速排序耗时');　　
     if (arr.length <= 1) {
         return arr;
     }　　
-    var pivotIndex = Math.floor(arr.length / 2);　　
-    var pivot = arr.splice(pivotIndex, 1)[0];　　
-    var left = [];　　
-    var right = [];　　
-    for (var i = 0; i < arr.length; i++) {　　　　
+    let pivotIndex = Math.floor(arr.length / 2);　　
+    let pivot = arr.splice(pivotIndex, 1)[0];　　
+    let left = [];　　
+    let right = [];　　
+    for (let i = 0; i < arr.length; i++) {　　　　
         if (arr[i] < pivot) {　　　　　　
             left.push(arr[i]);　　　　
         } else {　　　　　　
@@ -50,6 +50,6 @@ var quickSort2 = function(arr) {
     return quickSort2(left).concat([pivot], quickSort2(right));
 };
 
-var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+let arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 console.log(quickSort(arr, 0, arr.length - 1)); //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
 console.log(quickSort2(arr)); //[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
