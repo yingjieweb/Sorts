36428655ce98f1b215393d119c699997b9411d6d
yingjieweb <1184061078@qq.com>
2020-03-16 17:30:09 +0800

    'update'

--- sort/countingSort.js
+++ sort/countingSort.js
@@ -3,26 +3,50 @@
  * @param {Array} array 待排序数组
  * @return {Array}
  */
+//方法一
 function countingSort(array) {
-    var len = array.length,
+    let len = array.length,
         B = [],
         C = [],
         min = max = array[0];
     console.time('计数排序耗时');
-    for (var i = 0; i < len; i++) {
+    for (let i = 0; i < len; i++) {
         min = min <= array[i] ? min : array[i];
         max = max >= array[i] ? max : array[i];
         C[array[i]] = C[array[i]] ? C[array[i]] + 1 : 1;
     }
-    for (var j = min; j < max; j++) {
+    for (let j = min; j < max; j++) {
         C[j + 1] = (C[j + 1] || 0) + (C[j] || 0);
     }
-    for (var k = len - 1; k >= 0; k--) {
+    for (let k = len - 1; k >= 0; k--) {
         B[C[array[k]] - 1] = array[k];
         C[array[k]]--;
     }
     console.timeEnd('计数排序耗时');
     return B;
 }
-var arr = [2, 2, 3, 8, 7, 1, 2, 2, 2, 7, 3, 9, 8, 2, 1, 4, 2, 4, 6, 9, 2];
+let arr = [2, 2, 3, 8, 7, 1, 2, 2, 2, 7, 3, 9, 8, 2, 1, 4, 2, 4, 6, 9, 2];
 console.log(countingSort(arr)); //[1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 4, 4, 6, 7, 7, 8, 8, 9, 9]
+
+//方法二
+let countSort = arr =>{
+    let hashTable = {}, max = 0, result = [];
+    for(let i=0; i<arr.length; i++){ // 遍历数组
+        if(!(arr[i] in hashTable)){ // 视频中这一行写错
+            hashTable[arr[i]] = 1;
+        }else{
+            hashTable[arr[i]] += 1;
+        }
+        if(arr[i] > max) {max = arr[i]};
+    }
+    for(let j=0; j<=max; j++){ // 遍历哈希表
+        if(j in hashTable){
+            for(let i = 0; i<hashTable[j]; i++){
+                result.push(j);
+            }
+        }
+    }
+    return result
+}
+let arr = [2, 2, 3, 8, 7, 1, 2, 2, 2, 7, 3, 9, 8, 2, 1, 4, 2, 4, 6, 9, 2];
+console.log(countSort(arr)); //[1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 4, 4, 6, 7, 7, 8, 8, 9, 9]
--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -4,18 +4,18 @@
  * @return {Array}
  */
 function mergeSort(arr) { //采用自上而下的递归方法
-    var len = arr.length;
+    let len = arr.length;
     if (len < 2) {
         return arr;
     }
-    var middle = Math.floor(len / 2),
+    let middle = Math.floor(len / 2),
         left = arr.slice(0, middle),
         right = arr.slice(middle);
     return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-    var result = [];
+    let result = [];
     console.time('归并排序耗时');
     while (left.length && right.length) {
         if (left[0] <= right[0]) {
@@ -33,5 +33,5 @@ function merge(left, right) {
     console.timeEnd('归并排序耗时');
     return result;
 }
-var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+let arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 console.log(mergeSort(arr));
